6df4eaa8b30fc257d218a1fa37242cc4037732a8
Jeroen Wilmes <58515763+jpm-wilmes@users.noreply.github.com>
2024-08-01 10:51:47 +0200

    Aanpassingen

--- berichtwim.php
+++ berichtwim.php
@@ -8,19 +8,20 @@
                                    bericht3 = '" . $_POST['nieuwBericht3'] . "' WHERE id=1"; // er is maar 1 record. id=1
         $result=getData($sql);
     }
-    $sql="SELECT * FROM berichten";
+    $sql="SELECT * FROM berichten"; // er is maar 1 set dus behoeft geen verdere controle
     $berichten=getData($sql);
 ?>
-<!-- eenvoudig formulier om content te wijzigen uit de database (update). Overdracht met POST -->
+<!-- eenvoudig formulier om content te wijzigen uit de database (update). Overdracht met POST
+ Gebruik index 0 want er is maar 1 record. -->
 <!DOCTYPE html>
-    <head>
+    <head>   
     </head>
-    <body>
+    <body style="font-family: Arial">
         Wijzig een of meerdere berichten en druk op OK. Compacte teksten gebruiken<BR>
         <form method="post" action="berichtwim.php"> 
-            <input type="text" name="nieuwBericht1" value="<?php echo $berichten[0]['bericht1']?>"><br>
-            <input type="text" name="nieuwBericht2" value="<?php echo $berichten[0]['bericht2']?>"><br>
-            <input type="text" name="nieuwBericht3" value="<?php echo $berichten[0]['bericht3']?>"><br>
+            <input type="text" name="nieuwBericht1" size=50 value="<?php echo $berichten[0]['bericht1']?>"><br> 
+            <input type="text" name="nieuwBericht2" size=50 value="<?php echo $berichten[0]['bericht2']?>"><br>
+            <input type="text" name="nieuwBericht3" size=50 value="<?php echo $berichten[0]['bericht3']?>"><br>
             <button type="submit" name="ingestuurd"> OK </button>
         </form>
     </body>
